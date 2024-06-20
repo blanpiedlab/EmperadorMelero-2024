abc47432f524e561f5c1641fd03638f5d0b40b2b
blanpiedlab <150059775+blanpiedlab@users.noreply.github.com>
2024-06-20 17:20:46 -0400

    Update clusterFilterV060.m

--- SMLM-analysis/clusterFilterV060.m
+++ SMLM-analysis/clusterFilterV060.m
@@ -1,4 +1,9 @@
 %% [in, out, dbs_hs] = clusterFilter(dbscanfile,clusterfile,std_range,varargin)
+% “Distinct active zone protein machineries mediate Ca2+ channel clustering and vesicle priming at hippocampal synapses”
+% Emperador-Melero et al, 2024
+% authors: Aaron D Levy
+% copyright: 2024 Blanpied Lab, Dept of Physiology, University of Maryland School of Medicine
+%
 % Function clusterFilter will take Picasso DBSCAN output and filter the
 % clusters based on mean_frame and std_frame to remove clusters of spurious
 % binding.
