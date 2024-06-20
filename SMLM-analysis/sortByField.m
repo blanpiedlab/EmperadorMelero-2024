1c5aadb3557207216f620a9631798679dc2c698e
blanpiedlab <150059775+blanpiedlab@users.noreply.github.com>
2024-06-20 17:31:27 -0400

    Update sortByField.m

--- SMLM-analysis/sortByField.m
+++ SMLM-analysis/sortByField.m
@@ -1,4 +1,8 @@
 %% out = sortByField(in,sortcol)
+% “Distinct active zone protein machineries mediate Ca2+ channel clustering and vesicle priming at hippocampal synapses”
+% Emperador-Melero et al, 2024
+% authors: Aaron D Levy
+% copyright: 2024 Blanpied Lab, Dept of Physiology, University of Maryland School of Medicine
 % sorts an input loc table loaded from Omniloader (in) by the
 % column sortcol into a cell array
 function out = sortByField(in,sortcol)
